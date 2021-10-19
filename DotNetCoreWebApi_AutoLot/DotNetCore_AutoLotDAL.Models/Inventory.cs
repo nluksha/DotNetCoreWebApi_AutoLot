4d282fc4e81b845e712f1b86e66a1211d48c034a
Nikita Luksha <nikita.luksha@softensity.com>
2021-10-19 20:12:27 +0300

    Add DAL

---
+++ DotNetCoreWebApi_AutoLot/DotNetCore_AutoLotDAL.Models/Inventory.cs
@@ -0,0 +1,24 @@
+﻿using DotNetCore_AutoLotDAL.Models.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace DotNetCore_AutoLotDAL.Models
+{
+    [Table("Inventory")]
+    public partial class Inventory : EntityBase
+    {
+        [Required, StringLength(50)]
+        public string Make { get; set; }
+
+        [Required, StringLength(50)]
+        public string Color { get; set; }
+
+        [Required, StringLength(50)]
+        public string PetName { get; set; }
+
+        public List<Order> Orders { get; set; } = new List<Order>();
+    }
+}
